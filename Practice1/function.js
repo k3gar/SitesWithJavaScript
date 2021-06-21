0669aed015d9e1f344a7d872788cd42896c67392
Kevin Garcia <kevinede40@gmail.com>
2021-06-21 15:39:29 -0600

    Ejercicios

---
+++ Practice1/function.js
@@ -0,0 +1,24 @@
+var prueba = prompt("Enter your name")
+document.getElementById("Cambio").innerText = prueba
+
+
+
+/* var persona = null;
+var carro = "Vehículo"
+
+function mensaje() {
+    persona = document.getElementById('input_one');
+} */
+
+/* var persona = null;
+var carro = "Vehículo"
+
+function mensaje() {
+    persona = document.getElementById('input_one');
+    alert('Hola, tu nombre es ' + persona.value)
+} */
+
+/* function mensaje() {
+    persona = document.getElementById('input_one');
+    console.log('Hola, tu nombre es ' + persona.value)
+} */
